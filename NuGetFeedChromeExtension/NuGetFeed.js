e5512ae03f8ea84bd521f82c942c25bd86ff751d
ThomasArdal <thomasardal@gmail.com>
2011-09-14 14:05:05 +0200

    New chrome extension

--- NuGetFeedChromeExtension/NuGetFeed.js
+++ NuGetFeedChromeExtension/NuGetFeed.js
@@ -2,5 +2,5 @@ var link = document.createElement("link");
 link.setAttribute("rel", "alternate");
 link.setAttribute("type", "application/rss+xml");
 link.setAttribute("title", "Recent releases of X");
-link.setAttribute("href", "http://nugetfeed.org/List/Packages/X");
+link.setAttribute("href", document.location.href.replace('nuget.org', 'nugetfeed.apphb.com'));
 document.getElementsByTagName('head')[0].appendChild(link);
