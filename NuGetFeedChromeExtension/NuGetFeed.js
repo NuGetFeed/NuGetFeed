f73dfbc6be4d38e2587236a484c6f6ccc48d383d
ThomasArdal <thomasardal@gmail.com>
2011-09-15 11:35:11 +0200

    New version of chrome plugin

--- NuGetFeedChromeExtension/NuGetFeed.js
+++ NuGetFeedChromeExtension/NuGetFeed.js
@@ -1,6 +1,6 @@
 var link = document.createElement("link");
 link.setAttribute("rel", "alternate");
 link.setAttribute("type", "application/rss+xml");
-link.setAttribute("title", "Recent releases of X");
-link.setAttribute("href", document.location.href.replace('nuget.org', 'nugetfeed.apphb.com'));
+link.setAttribute("title", "Recent releases of " + document.location.href);
+link.setAttribute("href", document.location.href.replace('nuget.org', 'nugetfeed.org'));
 document.getElementsByTagName('head')[0].appendChild(link);
