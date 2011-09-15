d14939839798857a72c8fe33c698a133102361b8
Christian Pedersen <chripede@gmail.com>
2011-09-15 12:47:49 +0200

    New features for chrome plugin

--- NuGetFeedChromeExtension/NuGetFeed.js
+++ NuGetFeedChromeExtension/NuGetFeed.js
@@ -4,3 +4,5 @@ link.setAttribute("type", "application/rss+xml");
 link.setAttribute("title", "Recent releases of " + document.location.href);
 link.setAttribute("href", document.location.href.replace('nuget.org', 'nugetfeed.org'));
 document.getElementsByTagName('head')[0].appendChild(link);
+
+$('ul.links').append('<li><img src="http://nugetfeed.org/Content/Images/logo_10x10.png"> <a href="' + document.location.href.replace('nuget.org', 'nugetfeed.org') + '">NuGet feed</a></li>');
