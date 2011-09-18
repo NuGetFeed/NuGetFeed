a0a53f4ed9d34e67de3d2ec1d92f5cd732749cde
ThomasArdal <thomasardal@gmail.com>
2011-09-18 20:14:06 +0200

    Fixed NuGet feed to NuGet Feed

--- NuGetFeedChromeExtension/NuGetFeed.js
+++ NuGetFeedChromeExtension/NuGetFeed.js
@@ -5,4 +5,4 @@ link.setAttribute("title", "Recent releases of " + document.location.href);
 link.setAttribute("href", document.location.href.replace('nuget.org', 'nugetfeed.org'));
 document.getElementsByTagName('head')[0].appendChild(link);
 
-$('ul.links').append('<li><img src="http://nugetfeed.org/Content/Images/logo_10x10.png"> <a href="' + document.location.href.replace('nuget.org', 'nugetfeed.org') + '">NuGet feed</a></li>');
+$('ul.links').append('<li><img src="http://nugetfeed.org/Content/Images/logo_10x10.png"> <a href="' + document.location.href.replace('nuget.org', 'nugetfeed.org') + '">NuGet Feed</a></li>');
