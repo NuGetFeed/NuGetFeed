f2bc1de3cd1215d863470a7cf0c9503b1421d0a1
ThomasArdal <thomasardal@gmail.com>
2011-09-19 10:58:54 +0200

    Added VS extension

---
+++ VsExtension/NuGetPackage.cs
@@ -0,0 +1,103 @@
+namespace NuGetFeed.VSExtension {
+    using System;
+    using System.ComponentModel.Design;
+    using System.Runtime.InteropServices;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Xml.Linq;
+
+    using EnvDTE;
+
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", ProductVersion, IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideBindingPath] // Definition dll needs to be on VS binding path
+    [Guid(GuidList.guidNuGetPkgString)]
+    public sealed class NuGetPackage : Package
+    {
+        // This product version will be updated by the build script to match the daily build version.
+        // It is displayed in the Help - About box of Visual Studio
+        public const string ProductVersion = "1.2.0.0";
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initilaization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize() {
+            base.Initialize();
+
+            // Add our command handlers for menu (commands must exist in the .vsct file)
+            AddMenuCommandHandlers();
+        }
+
+        private void AddMenuCommandHandlers()
+        {
+            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null != mcs)
+            {
+                // menu command for opening Manage NuGet packages dialog
+                CommandID managePackageDialogCommandID = new CommandID(GuidList.guidNuGetDialogCmdSet, PkgCmdIDList.cmdidAddPackageDialog);
+                OleMenuCommand managePackageDialogCommand = new OleMenuCommand(Execute, null, this.BeforeExecute, managePackageDialogCommandID);
+                mcs.AddCommand(managePackageDialogCommand);
+            }
+        }
+
+        public void VSProjectFileProps2()
+        {
+            var dte = (DTE)GetService(typeof(DTE));
+
+            try
+            {
+                // Open a Visual C# or Visual Basic project
+                // before running this add-in.
+                Project project;
+                ProjectItems projItems;
+                ProjectItem projItem;
+                Property prop;
+                project = dte.Solution.Projects.Item(1);
+                projItems = project.ProjectItems;
+                for(int i = 1 ; i <= projItems.Count; i++ )
+                {
+                    projItem = projItems.Item(i);
+                    prop = projItem.Properties.Item("FileName");
+                    if (!prop.Value.ToString().ToLower().Equals("packages.config")) continue;
+
+                    XDocument xml = XDocument.Load(projItem.Properties.Item("FullPath").Value);
+                    var packages = new List<string>();
+                    foreach (var descendant in xml.Descendants("package"))
+                    {
+                        packages.Add(descendant.Attribute("id").Value);
+                    }
+
+                    if (packages.Count < 1) continue;
+
+                    var sb = new StringBuilder();
+                    foreach (var s in packages)
+                    {
+                        sb.Append(s).Append(",");
+                    }
+
+                    var param = sb.ToString().TrimEnd(',');
+                    System.Diagnostics.Process.Start("http://nugetfeed.org/List/Packages/" + param);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void BeforeExecute(object sender, EventArgs args)
+        {
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            VSProjectFileProps2();
+        }
+    }
+}
---
+++ VsExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGetFeed.VSExtension")]
+[assembly: AssemblyDescription("NuGet Feed for Visual Studio")]
+
+
+[assembly: AssemblyCompany("NuGetFeed.org")]
+[assembly: AssemblyProduct("NuGetFeed for Visual Studio")]
+[assembly: AssemblyCopyright("\x00a9 NuGetFeed.org. All rights reserved.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+
+#if !FIXED_ASSEMBLY_VERSION
+[assembly: AssemblyVersion("1.0.0.0")]
+#endif
+
+[assembly: NeutralResourcesLanguage("en-US")]
