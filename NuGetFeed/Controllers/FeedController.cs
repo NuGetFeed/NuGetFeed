9359159910bc6f04b87ce4d2a06d5e3740df65c8
ThomasArdal <thomasardal@gmail.com>
2011-09-28 19:59:59 +0200

    Made a new post method for adding all packages in a packages.config file to MyFeed

--- NuGetFeed/Controllers/FeedController.cs
+++ NuGetFeed/Controllers/FeedController.cs
@@ -10,6 +10,10 @@ using NuGetFeed.ViewModels;
 
 namespace NuGetFeed.Controllers
 {
+    using System.Xml.Linq;
+
+    using NuGetFeed.Infrastructure.ModelBinders;
+
     public class FeedController : Controller
     {
         private readonly UserRepository _userRepository;
@@ -26,6 +30,12 @@ namespace NuGetFeed.Controllers
         [Authorize]
         public ActionResult Index()
         {
+            if (TempData.ContainsKey("Message"))
+            {
+                ViewBag.MessageType = TempData.ContainsKey("MessageType") ? TempData["MessageType"] : "success";
+                ViewBag.Message = TempData["Message"];
+            }
+
             var context = new GalleryFeedContext(new Uri("http://packages.nuget.org/v1/FeedService.svc/"));
 
             var currentUser = _userRepository.GetByUsername(User.Identity.Name);
@@ -51,6 +61,50 @@ namespace NuGetFeed.Controllers
                                 };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Index([ModelBinder(typeof(XmlModelBinder))] XDocument packagesConfig)
+        {
+            if (packagesConfig == null)
+            {
+                TempData["Message"] = "packages.config could not be found";
+                TempData["MessageType"] = "error";
+                return RedirectToAction("Index");
+            }
+
+            List<string> packages;
+            try
+            {
+                packages = packagesConfig
+                    .Descendants("package")
+                    .Select(descendant => descendant.Attribute("id").Value)
+                    .ToList();
+            }
+            catch
+            {
+                TempData["Message"] = "Error during parsing packages.config occurred";
+                TempData["MessageType"] = "error";
+                return RedirectToAction("Index");
+            }
+
+            if (packages.Count < 1)
+            {
+                TempData["Message"] = "packages.config does not contain any packages";
+                TempData["MessageType"] = "warning";
+                return RedirectToAction("Index");
+            }
+
+            foreach (var id in packages)
+            {
+                this.AddToMyFeed(id);
+            }
+
+            TempData["Message"] = string.Format(
+                "{0} package{1} successfully added", packages.Count, packages.Count == 1 ? string.Empty : "s");
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult Rss(string id)
---
+++ NuGetFeed/Infrastructure/ModelBinders/XmlModelBinder.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace NuGetFeed.Infrastructure.ModelBinders
+{
+    public class XmlModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            return XDocument.Load(controllerContext.HttpContext.Request.InputStream);
+        }
+    }
+}
