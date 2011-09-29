62ba992ea0d1437a83fb54634982fdc33a920941
ThomasArdal <thomasardal@gmail.com>
2011-09-29 20:43:39 +0200

    Implemented favorites in Chrome Extension list content script using My Feed feature

--- NuGetFeedChromeExtension/NuGetFeedList.js
+++ NuGetFeedChromeExtension/NuGetFeedList.js
@@ -1,5 +1,8 @@
-$.each($('section > ul > li'), function(index, value) {
+$.each($('section > ul > li'), function (index, value) {
     var name = $(value).find('a:first').attr('href');
-    $(value).find('ul.packageMeta').append('<li><img src="http://nugetfeed.org/Content/Images/logo_10x10.png"> <a href="http://nugetfeed.org' + name + '">NuGet Feed</a></li>');
+    var id = name.substring(1 + name.lastIndexOf('/'));
+    var packageMeta = $(value).find('ul.packageMeta');
+    packageMeta.append($('<li>')
+        .append('<a href="http://nugetfeed.org' + name + '" target="_blank" title="NuGet Feed"><img src="http://nugetfeed.org/Content/Images/logo_16x16.png"/></a>')
+        .append('<a href="http://nugetfeed.org/feed/addtomyfeed/' + id + '" target="_blank" title="Add to My Feed"><img src="http://nugetfeed.org/Content/Images/star.png"/></a>'));
 });
-
