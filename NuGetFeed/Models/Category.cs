06ae3d6d7a3c7dab27d74df64f49345fd707cc53
ThomasArdal <thomasardal@gmail.com>
2011-10-11 07:47:38 +0200

    Fixed references
    Initial commit on new category view
    
    Added new url to global asax
    
    Added some error handling on CategoryController
    
    Added missing property
    
    Fixed bug in mapping packages
    
    Fixed styling issues on category

---
+++ NuGetFeed/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿namespace NuGetFeed.Models
+{
+    using System.Collections.Generic;
+
+    using Norm;
+
+    public class Category
+    {
+        [MongoIdentifier]
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string VisibleName { get; set; }
+
+        public string Description { get; set; }
+
+        public string SourceUrl { get; set; }
+
+        public string IconUrl { get; set; }
+
+        public List<string> Packages { get; protected set; }
+
+        public Category()
+        {
+            Packages = new List<string>();
+        }
+    }
+}
---
+++ NuGetFeed/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace NuGetFeed.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class CategoryViewModel
+    {
+        public CategoryViewModel()
+        {
+            this.PackageViewModels = new List<PackageViewModel>();
+        }
+
+        public string VisibleName { get; set; }
+
+        public string Description { get; set; }
+
+        public string SourceUrl { get; set; }
+
+        public string IconUrl { get; set; }
+
+        public List<PackageViewModel> PackageViewModels { get; protected set; }
+    }
+}
